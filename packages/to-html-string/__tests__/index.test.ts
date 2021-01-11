5094c5abdf423a271448e4109f13377ca3d3fd58
Stefano Verna <s.verna@datocms.com>
2021-01-11 14:28:01 +0100

    Implement to-html-string

--- packages/to-html-string/__tests__/index.test.ts
+++ packages/to-html-string/__tests__/index.test.ts
@@ -1,7 +1,201 @@
-'use strict';
+import {
+  render,
+  StructuredTextGraphQlResponse,
+  RenderError,
+  renderRule,
+} from '../src/';
+import { isHeading } from 'datocms-structured-text-utils';
 
-const toHtmlString = require('..');
+describe('StructuredText', () => {
+  describe('with no value', () => {
+    it('renders null', () => {
+      expect(render({ structuredText: null })).toMatchSnapshot();
+    });
+  });
 
-describe('to-html-string', () => {
-  it('needs tests');
+  describe('simple dast with no links/blocks', () => {
+    const structuredText: StructuredTextGraphQlResponse = {
+      value: {
+        schema: 'dast',
+        document: {
+          type: 'root',
+          children: [
+            {
+              type: 'heading',
+              level: 1,
+              children: [
+                {
+                  type: 'span',
+                  value: 'This\nis a\ntitle!',
+                },
+              ],
+            },
+          ],
+        },
+      },
+    };
+
+    describe('with default rules', () => {
+      it('renders the document', () => {
+        expect(render({ structuredText })).toMatchSnapshot();
+      });
+    });
+
+    describe('with custom rules', () => {
+      it('renders the document', () => {
+        expect(
+          render({
+            structuredText,
+            renderText: (text) => {
+              return text.replace(/This/, 'That');
+            },
+            customRules: [
+              renderRule(
+                isHeading,
+                ({ adapter: { renderNode }, node, children, key }) => {
+                  return renderNode(`h${node.level + 1}`, { key }, children);
+                },
+              ),
+            ],
+          }),
+        ).toMatchSnapshot();
+      });
+    });
+  });
+
+  describe('with links/blocks', () => {
+    type QuoteRecord = {
+      id: string;
+      __typename: 'QuoteRecord';
+      quote: string;
+      author: string;
+    };
+
+    type DocPageRecord = {
+      id: string;
+      __typename: 'DocPageRecord';
+      slug: string;
+      title: string;
+    };
+
+    const structuredText: StructuredTextGraphQlResponse<
+      QuoteRecord | DocPageRecord
+    > = {
+      value: {
+        schema: 'dast',
+        document: {
+          type: 'root',
+          children: [
+            {
+              type: 'heading',
+              level: 1,
+              children: [
+                {
+                  type: 'span',
+                  value: 'This is a',
+                },
+                {
+                  type: 'span',
+                  marks: ['highlight'],
+                  value: 'title',
+                },
+                {
+                  type: 'inlineItem',
+                  item: '123',
+                },
+                {
+                  type: 'itemLink',
+                  item: '123',
+                  children: [{ type: 'span', value: 'here!' }],
+                },
+              ],
+            },
+            {
+              type: 'block',
+              item: '456',
+            },
+          ],
+        },
+      },
+      blocks: [
+        {
+          id: '456',
+          __typename: 'QuoteRecord',
+          quote: 'Foo bar.',
+          author: 'Mark Smith',
+        },
+      ],
+      links: [
+        {
+          id: '123',
+          __typename: 'DocPageRecord',
+          title: 'How to code',
+          slug: 'how-to-code',
+        },
+      ],
+    };
+
+    describe('with default rules', () => {
+      it('renders the document', () => {
+        expect(
+          render({
+            structuredText,
+            renderInlineRecord: ({ adapter, record }) => {
+              switch (record.__typename) {
+                case 'DocPageRecord':
+                  return adapter.renderNode(
+                    'a',
+                    { href: `/docs/${record.slug}` },
+                    record.title,
+                  );
+                default:
+                  return null;
+              }
+            },
+            renderLinkToRecord: ({ record, children, adapter }) => {
+              switch (record.__typename) {
+                case 'DocPageRecord':
+                  return adapter.renderNode(
+                    'a',
+                    { href: `/docs/${record.slug}` },
+                    children,
+                  );
+                default:
+                  return null;
+              }
+            },
+            renderBlock: ({ record, adapter }) => {
+              switch (record.__typename) {
+                case 'QuoteRecord':
+                  return adapter.renderNode(
+                    'figure',
+                    null,
+                    adapter.renderNode('blockquote', null, record.quote),
+                    adapter.renderNode('figcaption', null, record.author),
+                  );
+                default:
+                  return null;
+              }
+            },
+          }),
+        ).toMatchSnapshot();
+      });
+    });
+
+    describe('with missing renderInlineRecord prop', () => {
+      it('raises an error', () => {
+        expect(() => {
+          render({ structuredText });
+        }).toThrow(RenderError);
+      });
+    });
+
+    describe('with missing record', () => {
+      it('raises an error', () => {
+        expect(() => {
+          render({ structuredText, renderInlineRecord: () => null });
+        }).toThrow(RenderError);
+      });
+    });
+  });
 });
--- packages/to-html-string/src/index.ts
+++ packages/to-html-string/src/index.ts
@@ -1,111 +1,207 @@
 import {
+  render as genericHtmlRender,
+  renderRule,
+} from 'datocms-structured-text-generic-html-renderer';
+import {
   Adapter,
-  isBlockquote,
-  isCode,
-  isHeading,
-  isLink,
-  isList,
-  isListItem,
-  isParagraph,
-  isRoot,
-  isSpan,
-  Mark,
-  Record,
-  render as genericRender,
+  isBlock,
+  isInlineItem,
+  isItemLink,
+  Record as StructuredTextGraphQlResponseRecord,
+  RenderError,
   RenderResult,
   RenderRule,
-  renderRule,
-  StructuredText,
-  TrasformFn,
-  RenderError,
+  StructuredText as StructuredTextGraphQlResponse,
 } from 'datocms-structured-text-utils';
+import vhtml from 'vhtml';
 
-export { renderRule, RenderError };
+export {
+  renderRule,
+  RenderError,
+  StructuredTextGraphQlResponse,
+  StructuredTextGraphQlResponseRecord,
+};
 
-export function markToTagName(mark: Mark): string {
-  switch (mark) {
-    case 'emphasis':
-      return 'em';
-    case 'underline':
-      return 'u';
-    case 'strikethrough':
-      return 'strike';
-    case 'highlight':
-      return 'mark';
-    default:
-      return mark;
+type AdapterReturn = string | null;
+
+const vhtmlAdapter = (
+  tagName: string,
+  attrs?: Record<string, string> | null,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  ...children: any[]
+): AdapterReturn => {
+  if (attrs) {
+    delete attrs.key;
   }
-}
 
-export function render<
-  R extends Record,
-  H extends TrasformFn,
-  T extends TrasformFn,
-  M extends TrasformFn,
-  F extends TrasformFn
->(
-  adapter: Adapter<H, T, M, F>,
-  structuredText: StructuredText<R>,
-  customRules: RenderRule<H, T, M, F>[],
-): RenderResult<H, T, M, F> {
-  return genericRender(adapter, structuredText, [
-    ...customRules,
-    renderRule(isRoot, ({ adapter: { renderFragment }, key, children }) => {
-      return renderFragment(children, key);
-    }),
-    renderRule(isParagraph, ({ adapter: { renderNode }, key, children }) => {
-      return renderNode('p', { key }, children);
-    }),
-    renderRule(isList, ({ adapter: { renderNode }, node, key, children }) => {
-      return renderNode(
-        node.style === 'bulleted' ? 'ul' : 'ol',
-        { key },
-        children,
-      );
-    }),
-    renderRule(isListItem, ({ adapter: { renderNode }, key, children }) => {
-      return renderNode('li', { key }, children);
-    }),
-    renderRule(isBlockquote, ({ adapter: { renderNode }, key, children }) => {
-      return renderNode('blockquote', { key }, children);
-    }),
-    renderRule(isCode, ({ adapter: { renderNode, renderText }, key, node }) => {
-      return renderNode(
-        'pre',
-        { key, 'data-language': node.language },
-        renderNode('code', null, renderText(node.code)),
-      );
-    }),
-    renderRule(isLink, ({ adapter: { renderNode }, key, children, node }) => {
-      return renderNode('a', { key, href: node.url }, children);
-    }),
-    renderRule(
-      isHeading,
-      ({ node, adapter: { renderNode }, children, key }) => {
-        return renderNode(`h${node.level}`, { key }, children);
-      },
-    ),
-    renderRule(isSpan, ({ adapter: { renderNode, renderText }, key, node }) => {
-      const marks = node.marks || [];
+  return vhtml(tagName, attrs, ...children);
+};
 
-      const lines = node.value.split(/\n/);
+export const defaultAdapter = {
+  renderNode: vhtmlAdapter,
+  renderMark: vhtmlAdapter,
+  renderFragment: (children: AdapterReturn[]): AdapterReturn =>
+    children.join(''),
+  renderText: (text: string): AdapterReturn => text,
+};
 
-      const textWithNewlinesConvertedToBr =
-        lines.length > 0
-          ? lines.slice(1).reduce(
-              (acc, line, index) => {
-                return acc.concat([
-                  renderNode('br', { key: `br-${index}` }),
-                  renderText(line, `line-${index}`),
-                ]);
-              },
-              [renderText(lines[0], 'line-first')],
-            )
-          : renderText(node.value, key);
+type H = typeof defaultAdapter.renderNode;
+type M = typeof defaultAdapter.renderMark;
+type T = typeof defaultAdapter.renderText;
+type F = typeof defaultAdapter.renderFragment;
 
-      return marks.reduce<RenderResult<H, T, M, F>>((children, mark) => {
-        return renderNode(markToTagName(mark), { key }, children);
-      }, textWithNewlinesConvertedToBr);
-    }),
-  ]);
+type RenderInlineRecordContext<
+  R extends StructuredTextGraphQlResponseRecord
+> = {
+  record: R;
+  adapter: Adapter<H, T, M, F>;
+};
+
+type RenderRecordLinkContext<R extends StructuredTextGraphQlResponseRecord> = {
+  record: R;
+  adapter: Adapter<H, T, M, F>;
+  children: RenderResult<H, T, M, F>;
+};
+
+type RenderBlockContext<R extends StructuredTextGraphQlResponseRecord> = {
+  record: R;
+  adapter: Adapter<H, T, M, F>;
+};
+
+export type StructuredTextPropTypes<
+  R extends StructuredTextGraphQlResponseRecord
+> = {
+  /** The actual field value you get from DatoCMS **/
+  structuredText: StructuredTextGraphQlResponse<R> | null | undefined;
+  /** A set of additional rules to convert the document to HTML **/
+  customRules?: RenderRule<H, T, M, F>[];
+  /** Fuction that converts an 'inlineItem' node into an HTML string **/
+  renderInlineRecord?: (context: RenderInlineRecordContext<R>) => string | null;
+  /** Fuction that converts an 'itemLink' node into an HTML string **/
+  renderLinkToRecord?: (context: RenderRecordLinkContext<R>) => string | null;
+  /** Fuction that converts a 'block' node into an HTML string **/
+  renderBlock?: (context: RenderBlockContext<R>) => string | null;
+  /** Fuction that converts a simple string text into an HTML string **/
+  renderText?: T;
+  /** React.createElement-like function to use to convert a node into an HTML string **/
+  renderNode?: H;
+  /** React.createElement-like function to use to convert a mark into an HTML string **/
+  renderMark?: M;
+  /** Function to use to generate a React.Fragment **/
+  renderFragment?: F;
+};
+
+export function render<R extends StructuredTextGraphQlResponseRecord>({
+  structuredText,
+  renderInlineRecord,
+  renderLinkToRecord,
+  renderBlock,
+  renderText,
+  renderNode,
+  renderMark,
+  renderFragment,
+  customRules,
+}: StructuredTextPropTypes<R>): string | null {
+  if (!structuredText) {
+    return null;
+  }
+
+  const result = genericHtmlRender(
+    {
+      renderText: renderText || defaultAdapter.renderText,
+      renderNode: renderNode || defaultAdapter.renderNode,
+      renderMark: renderMark || defaultAdapter.renderMark,
+      renderFragment: renderFragment || defaultAdapter.renderFragment,
+    },
+    structuredText,
+    [
+      renderRule(isInlineItem, ({ node, adapter }) => {
+        if (!renderInlineRecord) {
+          throw new RenderError(
+            `The Structured Text document contains an 'inlineItem' node, but no 'renderInlineRecord' prop is specified!`,
+            node,
+          );
+        }
+
+        if (!structuredText.links) {
+          throw new RenderError(
+            `The Structured Text document contains an 'inlineItem' node, but .links is not present!`,
+            node,
+          );
+        }
+
+        const item = structuredText.links.find((item) => item.id === node.item);
+
+        if (!item) {
+          throw new RenderError(
+            `The Structured Text document contains an 'inlineItem' node, but cannot find a record with ID ${node.item} inside .links!`,
+            node,
+          );
+        }
+
+        return renderInlineRecord({ record: item, adapter });
+      }),
+      renderRule(isItemLink, ({ node, children, adapter }) => {
+        if (!renderLinkToRecord) {
+          throw new RenderError(
+            `The Structured Text document contains an 'itemLink' node, but no 'renderLinkToRecord' prop is specified!`,
+            node,
+          );
+        }
+
+        if (!structuredText.links) {
+          throw new RenderError(
+            `The Structured Text document contains an 'itemLink' node, but .links is not present!`,
+            node,
+          );
+        }
+
+        const item = structuredText.links.find((item) => item.id === node.item);
+
+        if (!item) {
+          throw new RenderError(
+            `The Structured Text document contains an 'itemLink' node, but cannot find a record with ID ${node.item} inside .links!`,
+            node,
+          );
+        }
+
+        return renderLinkToRecord({
+          record: item,
+          adapter,
+          // eslint-disable-next-line @typescript-eslint/no-explicit-any
+          children: (children as any) as ReturnType<F>,
+        });
+      }),
+      renderRule(isBlock, ({ node, adapter }) => {
+        if (!renderBlock) {
+          throw new RenderError(
+            `The Structured Text document contains a 'block' node, but no 'renderBlock' prop is specified!`,
+            node,
+          );
+        }
+
+        if (!structuredText.blocks) {
+          throw new RenderError(
+            `The Structured Text document contains a 'block' node, but .blocks is not present!`,
+            node,
+          );
+        }
+
+        const item = structuredText.blocks.find(
+          (item) => item.id === node.item,
+        );
+
+        if (!item) {
+          throw new RenderError(
+            `The Structured Text document contains a 'block' node, but cannot find a record with ID ${node.item} inside .blocks!`,
+            node,
+          );
+        }
+
+        return renderBlock({ record: item, adapter });
+      }),
+    ].concat(customRules || []),
+  );
+
+  return result;
 }
