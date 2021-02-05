8846e85ebc4c1f64a12a61f13797931f5aae80d8
Stefano Verna <s.verna@datocms.com>
2021-02-05 16:12:28 +0100

    Ignore warnings

--- packages/html-to-structured-text/src/preprocessors/google-docs.ts
+++ packages/html-to-structured-text/src/preprocessors/google-docs.ts
@@ -1,10 +1,11 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { findAll, visit } from 'unist-utils-core';
-import { HastRootNode, HastElementNode, HastNode } from '../types';
+import { HastRootNode, HastNode } from '../types';
 
 export default function preprocessGoogleDocs(tree: HastRootNode): void {
-  const gdocsBranches = findAll(tree, isGoogleDocsNode);
+  const gdocsBranches = findAll(tree as any, isGoogleDocsNode as any);
   gdocsBranches.forEach((branch) => {
-    visit(branch, (node: HastNode) => {
+    visit(branch, ((node: HastNode) => {
       if (
         node.type !== 'element' ||
         node.tagName !== 'span' ||
@@ -13,24 +14,24 @@ export default function preprocessGoogleDocs(tree: HastRootNode): void {
       ) {
         return;
       }
-      const markTags = [];
+      const markTags: string[] = [];
       node.properties.style.split(';').forEach((declaration) => {
-        let [prop, ...value] = declaration.split(':');
-        prop = prop.trim();
-        value = value.join(':').trim();
+        const [firstChunk, ...otherChunks] = declaration.split(':');
+        const prop = firstChunk.trim();
+        const gdocsMark = otherChunks.join(':').trim();
         switch (prop) {
           case 'font-weight':
-            if (value === 'bold' || Number(value) > 400) {
+            if (gdocsMark === 'bold' || Number(gdocsMark) > 400) {
               markTags.push('strong');
             }
             break;
           case 'font-style':
-            if (value === 'italic') {
+            if (gdocsMark === 'italic') {
               markTags.push('em');
             }
             break;
           case 'text-decoration':
-            if (value === 'underline') {
+            if (gdocsMark === 'underline') {
               markTags.push('u');
             }
             break;
@@ -48,20 +49,26 @@ export default function preprocessGoogleDocs(tree: HastRootNode): void {
           };
         });
       });
-    });
+    }) as any);
   });
 }
 
-function isGoogleDocsNode(node: HastNode, index, parent: HastNode): boolean {
-  const isGDocsNode: HastElementNode =
+function isGoogleDocsNode(
+  node: HastNode,
+  index: number,
+  parent: HastNode,
+): boolean {
+  const isGDocsNode =
     node.type === 'element' &&
     typeof node.properties === 'object' &&
     typeof node.properties.id === 'string' &&
     node.properties.id.startsWith('docs-internal-guid-');
 
   if (isGDocsNode) {
-    // Remove google docs tag.
-    parent.children.splice(index, 1, ...node.children);
+    if ('children' in parent && 'children' in node) {
+      // Remove google docs tag.
+      parent.children.splice(index, 1, ...node.children);
+    }
     return true;
   } else {
     return false;
