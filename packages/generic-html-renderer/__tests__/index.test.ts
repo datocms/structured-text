0dfeb0b6587d8d440f1d8f61b8a6b5ef54b30b08
Silvano Stralla <silvano.stralla@sistrall.it>
2024-08-06 17:53:24 +0900

    Change fixture text

--- packages/generic-html-renderer/__tests__/index.test.ts
+++ packages/generic-html-renderer/__tests__/index.test.ts
@@ -150,7 +150,7 @@ describe('render', () => {
               children: [
                 {
                   type: 'span',
-                  value: "And it's fucking perfect.",
+                  value: "And it's berry perfect.",
                 },
               ],
             },
@@ -160,7 +160,7 @@ describe('render', () => {
               children: [
                 {
                   type: 'span',
-                  value: 'Seriously, what the fuck else do you want?',
+                  value: 'Seriously, what the grape else do you want?',
                 },
               ],
             },
@@ -170,17 +170,17 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    'You probably build websites and think your 💩  is special.\nYou think your 13 megabyte parallax-ative home page is going to get you',
+                    'You probably build websites and think your banana is special.\nYou think your 13 megabyte parallax-ative home page is going to get you',
                 },
                 {
                   type: 'span',
                   marks: ['strong'],
-                  value: ' some fucking Awwward banner y',
+                  value: ' some fruity Awwward banner y',
                 },
                 {
                   type: 'span',
                   value:
-                    'ou can glue to the top corner of your site. You think your 40-pound jQuery file and 83 polyfills give IE7 a boner because it finally has box-shadow. Wrong, motherfucker. Let me describe your perfect-ass website:',
+                    'ou can glue to the top corner of your site. You think your 40-pound jQuery file and 83 polyfills give IE7 a peach because it finally has box-shadow. Wrong, melonhead. Let me describe your perfect-pear website:',
                 },
               ],
             },
@@ -196,7 +196,7 @@ describe('render', () => {
                       children: [
                         {
                           type: 'span',
-                          value: "Shit's lightweight and loads fast",
+                          value: "Fruit's lightweight and loads fast",
                         },
                       ],
                     },
@@ -213,7 +213,7 @@ describe('render', () => {
                                 {
                                   type: 'span',
                                   value:
-                                    'Looks the same in all your shitty browsers',
+                                    'Looks the same in all your citrusy browsers',
                                 },
                               ],
                             },
@@ -227,7 +227,7 @@ describe('render', () => {
                               children: [
                                 {
                                   type: 'span',
-                                  value: 'Fits on all your shitty screens',
+                                  value: 'Fits on all your juicy screens',
                                 },
                               ],
                             },
@@ -244,7 +244,7 @@ describe('render', () => {
                                         {
                                           type: 'span',
                                           value:
-                                            "The motherfucker's accessible to every asshole that visits your site",
+                                            "The kumquat's accessible to every apricot that visits your site",
                                         },
                                       ],
                                     },
@@ -267,7 +267,7 @@ describe('render', () => {
                         {
                           type: 'span',
                           value:
-                            "Shit's legible and gets your fucking point across (if you had one instead of just 5mb pics of hipsters drinking coffee)",
+                            "Fruit's legible and gets your juicy point across (if you had one instead of just 5mb pics of hipsters drinking coffee)",
                         },
                       ],
                     },
@@ -281,7 +281,7 @@ describe('render', () => {
               children: [
                 {
                   type: 'span',
-                  value: 'Well guess what, motherfucker:',
+                  value: 'Well guess what, you absolute kiwi:',
                 },
               ],
             },
@@ -291,7 +291,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    "You. Are. Over-designing. Look at this shit. It's a motherfucking website. Why the fuck do you need to animate a fucking trendy-ass banner flag when I hover over that useless piece of shit? You spent hours on it and added 80 kilobytes to your fucking site, and some motherfucker jabbing at it on their iPad with fat sausage fingers will never see that shit. Not to mention blind people will never see that shit, but they don't see any of your shitty shit.",
+                    "You. Are. Over-designing. Look at this fruit. It's a banana-loving website. Why the fig do you need to animate a fruity trendy-pear banner flag when I hover over that useless piece of mango? You spent hours on it and added 80 kilobytes to your juicy site, and some papaya jabbing at it on their iPad with fat sausage fingers will never see that fruit. Not to mention blind people will never see that fruit, but they don't see any of your juicy fruit.",
                 },
               ],
             },
@@ -311,7 +311,7 @@ describe('render', () => {
               children: [
                 {
                   type: 'span',
-                  value: "It's fucking lightweight",
+                  value: "It's fruity lightweight",
                 },
               ],
             },
@@ -321,7 +321,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    'This entire page weighs less than the gradient-meshed facebook logo on your fucking Wordpress site. Did you seriously load 100kb of jQuery UI just so you could animate the fucking background color of a div? You loaded all 7 fontfaces of a shitty webfont just so you could say "Hi." at 100px height at the beginning of your site? ',
+                    'This entire page weighs less than the gradient-meshed facebook logo on your juicy Wordpress site. Did you seriously load 100kb of jQuery UI just so you could animate the fruity background color of a div? You loaded all 7 fontfaces of a citrusy webfont just so you could say "Hi." at 100px height at the beginning of your site? ',
                 },
               ],
             },
@@ -330,7 +330,7 @@ describe('render', () => {
               children: [
                 {
                   type: 'span',
-                  value: 'You piece of shit.',
+                  value: 'You piece of fruit.',
                 },
               ],
             },
@@ -398,7 +398,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    "You dumbass. You thought you needed media queries to be responsive, but no. Responsive means that it responds to whatever motherfucking screensize it's viewed on. This site doesn't care if you're on an iMac or a motherfucking Tamagotchi.",
+                    "You kumquat. You thought you needed media queries to be responsive, but no. Responsive means that it responds to whatever fruity screensize it's viewed on. This site doesn't care if you're on an iMac or a banana-loving Tamagotchi.",
                 },
               ],
             },
@@ -408,7 +408,7 @@ describe('render', () => {
               children: [
                 {
                   type: 'span',
-                  value: 'It fucking works',
+                  value: 'It fruity works',
                 },
               ],
             },
@@ -418,7 +418,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    "Look at this shit. You can read it ... that is, if you can read, motherfucker. It makes sense. It has motherfucking hierarchy. It's using HTML5 tags so you and your bitch-ass browser know what the fuck's in this fucking site. That's semantics, motherfucker.",
+                    "Look at this fruit. You can read it ... that is, if you can read, melonhead. It makes sense. It has banana-loving hierarchy. It's using HTML5 tags so you and your pear-headed browser know what the fig's in this juicy site. That's semantics, you absolute kiwi.",
                 },
               ],
             },
@@ -428,7 +428,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    'It has content on the fucking screen. Your site has three bylines and link to your dribbble account, but you spread it over 7 full screens and make me click some bobbing button to show me how cool the jQuery ScrollTo plugin is.',
+                    'It has content on the fruity screen. Your site has three bylines and link to your dribbble account, but you spread it over 7 full screens and make me click some bobbing button to show me how cool the jQuery ScrollTo plugin is.',
                 },
               ],
             },
@@ -438,7 +438,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    'Cross-browser compatibility? Load this motherfucker in IE6. I fucking dare you.',
+                    'Cross-browser compatibility? Load this kumquat in IE6. I fruity dare you.',
                 },
               ],
             },
@@ -467,7 +467,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    "Like the man who's never grown out his beard has no idea what his true natural state is, you have no fucking idea what a website is. All you have ever seen are shitty skeuomorphic bastardizations of what should be text communicating a fucking message. This is a real, naked website. Look at it. It's fucking beautiful.",
+                    "Like the man who's never grown out his beard has no idea what his true natural state is, you have no fruity idea what a website is. All you have ever seen are citrusy skeuomorphic bastardizations of what should be text communicating a juicy message. This is a real, naked website. Look at it. It's fruity beautiful.",
                 },
               ],
             },
@@ -477,7 +477,7 @@ describe('render', () => {
               children: [
                 {
                   type: 'span',
-                  value: 'Yes, this is fucking satire, you fuck',
+                  value: 'Yes, this is fruity satire, you fig',
                 },
               ],
             },
@@ -487,13 +487,13 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    "I'm not actually saying your shitty site should look like this. What I'm saying is that all the problems we have with websites are ones we create ourselves. Websites aren't broken by default, they are functional, high-performing, and accessible. You break them. You son-of-a-bitch.",
+                    "I'm not actually saying your citrusy site should look like this. What I'm saying is that all the problems we have with websites are ones we create ourselves. Websites aren't broken by default, they are functional, high-performing, and accessible. You break them. You son-of-a-peach.",
                 },
               ],
             },
             {
               type: 'blockquote',
-              attribution: 'some German motherfucker',
+              attribution: 'some German fruithead',
               children: [
                 {
                   type: 'paragraph',
@@ -541,7 +541,7 @@ describe('render', () => {
                 {
                   type: 'span',
                   value:
-                    ' was created. You should try it today to make your own motherfucking websites.',
+                    ' was created. You should try it today to make your own banana-loving websites.',
                 },
               ],
             },
